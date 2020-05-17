2a8bdf37329ba8fefcf9b1b0003277a977dddb6d
Harivansh <hello@harivan.sh>
2020-05-17 23:01:18 +0400

    add validation

--- laravel.py
+++ laravel.py
@@ -2,10 +2,32 @@
 A script to automate the installation of Laravel projects on Docker.
 """
 
+import re
 import logging
 from argparse import ArgumentParser
 from collections import namedtuple
 from types import SimpleNamespace
+
+
+class Validation:
+    @staticmethod
+    def is_pascal_case(value: str) -> bool:
+        pascal_case_regex = re.compile(r'^[A-Z][a-z]+(?:[A-Z][a-z]+)*$')
+
+        return pascal_case_regex.match(value) is not None
+
+    @staticmethod
+    def domain_is_valid(domain: str) -> bool:
+        domain_regex = re.compile(
+            r'^'
+            r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?))'  # domain
+            r'(?:/?|[/?]\S+)'  # path
+            r'$',
+            re.IGNORECASE
+        )
+
+        return domain_regex.match(domain) is not None
+
 
 if __name__ == '__main__':
     logging.basicConfig(
@@ -51,4 +73,9 @@ if __name__ == '__main__':
     arguments = parser.main.parse_args()
 
     if arguments.action == 'setup':
-        print(arguments)
+        # validation
+        if not Validation.is_pascal_case(arguments.project_name):
+            raise RuntimeError(f'The project name: {arguments.project_name} is not pascal-cased.')
+
+        if not Validation.domain_is_valid(arguments.domain):
+            raise RuntimeError(f'The domain: {arguments.domain} is invalid.')
