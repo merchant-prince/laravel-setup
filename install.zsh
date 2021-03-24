10f88d194aaf4a5ce92c2c0c5ec4589422aaefd6
Harivansh <hello@harivan.sh>
2021-03-24 23:28:21 +0400

    refactor install script

--- install.zsh
+++ install.zsh
@@ -2,7 +2,7 @@
 
 
 REQUIRED_PYTHON_VERSION="3.9"
-RECOMMENDED_PIP_VERSION="20.3.3"
+RECOMMENDED_PIP_VERSION="20.3"
 PROJECT_PATH="$(dirname "$(realpath "${0}")")"
 VENV_PATH="${PROJECT_PATH}/.venv"
 
@@ -21,8 +21,8 @@ fi
 success "python3... OK"
 
 info "checking python's version"
-if [ "$(python3 -c "import sys; print(''.join([str(n) for n in sys.version_info[:2]]))")" -lt "$(echo "${REQUIRED_PYTHON_VERSION}" | tr --delete '.')" ]; then
-  error "python ${REQUIRED_PYTHON_VERSION} or higher is required to run this project."
+if [ "$(python3 -c "import sys; print(''.join([str(n) for n in sys.version_info[:2]]))")" -lt "${REQUIRED_PYTHON_VERSION//./}" ]; then
+  error "python ${REQUIRED_PYTHON_VERSION} or higher IS required to run this project."
   exit 64
 fi
 success "python's version... OK"
@@ -37,7 +37,7 @@ fi
 success "pip3... OK"
 
 info "checking pip's version"
-if [ "$(pip3 --version | cut --delimiter ' ' --fields 2 | cut --delimiter '.' --fields '1-2' | tr --delete '.')" -lt "$(echo "${RECOMMENDED_PIP_VERSION}" | cut --delimiter ' ' --fields 2 | cut --delimiter '.' --fields '1-2' | tr --delete '.')" ]; then
+if [ "$(pip3 --version | cut --delimiter ' ' --fields 2 | cut --delimiter '.' --fields '1-2' | tr --delete '.')" -lt "${RECOMMENDED_PIP_VERSION//./}" ]; then
   warning "pip ${RECOMMENDED_PIP_VERSION} or higher MIGHT be required to run this project."
 else
   success "pip's version... OK"
@@ -73,8 +73,12 @@ fi
 deactivate
 
 
-# post-install information
+# post-install
 echo ""
 echo "Add the following alias to your '.zshrc' file:"
+echo ""
 echo "alias laravel=\"${VENV_PATH}/bin/python ${PROJECT_PATH}/main.py\""
 echo ""
+echo ""
+
+success "project setup complete."
