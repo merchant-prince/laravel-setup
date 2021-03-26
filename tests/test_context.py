fa45025e97634a06b42a53913c1707cb10c9b484
Harivansh <hello@harivan.sh>
2021-03-26 21:57:21 +0400

    write tests for cd context manager

---
+++ tests/test_context.py
@@ -0,0 +1,34 @@
+import os
+import unittest
+
+from modules import context
+
+
+class CdTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.old_cwd = os.getcwd()
+        self.destination = '/tmp'
+
+        self.assertNotEqual(self.old_cwd, self.destination)
+
+    def test_changes_directory_context_successfully(self) -> None:
+        with context.cd(self.destination):
+            self.assertEqual(os.getcwd(), self.destination)
+
+        self.assertEqual(os.getcwd(), self.old_cwd)
+
+    def test_changes_directory_context_back_when_exception_is_raised_within_with_context(self) -> None:
+        try:
+            with context.cd(self.destination):
+                raise RuntimeError()
+        except RuntimeError:
+            self.assertEqual(os.getcwd(), self.old_cwd)
+
+    def test_does_not_change_directory_context_when_invalid_destination_is_passed(self) -> None:
+        non_existent_destination = '/NON/EXISTENT_DIRECTORY'
+
+        try:
+            with context.cd(non_existent_destination):
+                pass
+        except FileNotFoundError:
+            self.assertEqual(os.getcwd(), self.old_cwd)
