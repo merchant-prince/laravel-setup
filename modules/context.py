218e21952a40ff19184bbaaf30001d6430a13a31
Harivansh <hello@harivan.sh>
2021-03-26 20:49:00 +0400

    setup context module

---
+++ modules/context.py
@@ -0,0 +1,18 @@
+import os
+from contextlib import contextmanager
+
+
+@contextmanager
+def cd(destination: str) -> None:
+    """
+    A context manager to change directory. Mimics unix's cd.
+    Args:
+        destination (str): The directory to cd into.
+    """
+    cwd = os.getcwd()
+
+    try:
+        os.chdir(destination)
+        yield
+    finally:
+        os.chdir(cwd)
