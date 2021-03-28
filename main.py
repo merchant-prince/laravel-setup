d3965fc2b2248b6f5b6ceb709e678360d948f36b
Harivansh <hello@harivan.sh>
2021-03-29 03:44:39 +0400

    add project directory structure creation

--- main.py
+++ main.py
@@ -3,8 +3,8 @@
 from typing import Mapping
 
 from modules.extracts import parser
+from modules.scaffolding import directory_structure_is_valid, create_directory_structure
 from modules.validation import directory_exists, domain_is_valid, is_pascal_case
-
 
 if __name__ == '__main__':
     arguments = parser().parse_args()
@@ -13,7 +13,8 @@ if __name__ == '__main__':
         raise RuntimeError(f"The project name: '{arguments.project_name}' is not pascal-cased.")
 
     if directory_exists(arguments.project_name):
-        raise RuntimeError(f"The directory: '{arguments.project_name}' already exists in the current directory.")
+        raise RuntimeError(
+            f"The directory: '{arguments.project_name}' already exists in the current working directory.")
 
     if not domain_is_valid(arguments.domain):
         raise RuntimeError(f"The domain: '{arguments.domain}' is invalid.")
@@ -29,3 +30,44 @@ if __name__ == '__main__':
         'services.postgres.username': 'username',
         'services.postgres.password': 'password',
     }
+
+    project_directory_structure = {
+        configuration['project.name']: {
+            # docker-compose.yml
+            # .gitignore
+            # README.md
+
+            'configuration': {
+                'nginx': {
+                    'conf.d': {
+                        # default.conf
+                        # utils.conf
+                    },
+                    'ssl': {
+                        # key.pem
+                        # certificate.pem
+                    }
+                },
+                'supervisor': {
+                    'conf.d': {
+                        # supervisord.conf
+                    }
+                }
+            },
+            'docker-compose': {
+                'services': {
+                    'php': {
+                        # Dockerfile
+                    }
+                }
+            },
+            'application': {
+                # Laravel application
+            }
+        }
+    }
+
+    if not directory_structure_is_valid(project_directory_structure):
+        raise RuntimeError('The project directory structure is invalid.')
+
+    create_directory_structure(project_directory_structure)
