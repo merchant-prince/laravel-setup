303f98ea1fc913bad41f2611da34c910f529fa72
Harivansh <hello@harivan.sh>
2021-03-31 04:45:44 +0400

    setup docker-compse.yml generation

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
-from typing import Mapping
+from os import getuid, getgid
+from string import Template
+from typing import Mapping, Union
 
 from modules.extracts import parser, preliminary_checks, scaffold_project_directory_structure, validate_script_arguments
 from modules.scaffolding import generate_self_signed_tls_certificate
-from modules.utilities import cd
+from modules.utilities import cd, template_path
 
 if __name__ == '__main__':
     preliminary_checks(requirements={
@@ -16,7 +18,7 @@ if __name__ == '__main__':
 
     validate_script_arguments(arguments := parser().parse_args())
 
-    configuration: Mapping[str, str] = {
+    configuration: Mapping[str, Union[str, int]] = {
         'project.name': arguments.project_name,
         'project.domain': arguments.domain,
 
@@ -26,47 +28,48 @@ if __name__ == '__main__':
         'services.postgres.database': arguments.project_name.lower(),
         'services.postgres.username': 'username',
         'services.postgres.password': 'password',
+
+        'services.adminer.port': 8080,
+        'services.mailhog.port': 8025,
     }
 
-    scaffold_project_directory_structure(
-        project_directory_structure := {
-            configuration['project.name']: {
-                # docker-compose.yml
-                # .gitignore
-                # README.md
+    scaffold_project_directory_structure({
+        configuration['project.name']: {
+            # docker-compose.yml
+            # .gitignore
+            # README.md
 
-                'configuration': {
-                    'nginx': {
-                        'conf.d': {
-                            # default.conf
-                            # utils.conf
-                        },
-                        'ssl': {
-                            # key.pem
-                            # certificate.pem
-                        }
+            'configuration': {
+                'nginx': {
+                    'conf.d': {
+                        # default.conf
+                        # utils.conf
                     },
-                    'supervisor': {
-                        'conf.d': {
-                            # supervisord.conf
-                        }
+                    'ssl': {
+                        # key.pem
+                        # certificate.pem
                     }
                 },
-                'docker-compose': {
-                    'services': {
-                        'php': {
-                            # Dockerfile
-                        }
+                'supervisor': {
+                    'conf.d': {
+                        # supervisord.conf
                     }
-                },
-                'application': {
-                    'core': {
-                        # Laravel application
+                }
+            },
+            'docker-compose': {
+                'services': {
+                    'php': {
+                        # Dockerfile
                     }
+                }
+            },
+            'application': {
+                'core': {
+                    # Laravel application
                 }
             }
         }
-    )
+    })
 
     with cd(configuration['project.name']):
         with cd('configuration/nginx/ssl'):
@@ -74,4 +77,18 @@ if __name__ == '__main__':
                 certificate_name=configuration['services.nginx.ssl.certificate'],
                 key_name=configuration['services.nginx.ssl.key'],
                 domain=configuration['project.domain']
+            )
+
+        with open('docker-compose.yml', 'w') as file, open(f"{template_path('docker-compose.yml')}") as template:
+            file.write(
+                Template(template.read()).substitute({
+                    'PROJECT_NAME': configuration['project.name'],
+                    'USER_ID': getuid(),
+                    'GROUP_ID': getgid(),
+                    'POSTGRES_DB': configuration['services.postgres.database'],
+                    'POSTGRES_USER': configuration['services.postgres.username'],
+                    'POSTGRES_PASSWORD': configuration['services.postgres.password'],
+                    'ADMINER_PORT': configuration['services.adminer.port'],
+                    'MAILHOG_PORT': configuration['services.mailhog.port'],
+                })
             )
