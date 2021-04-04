b2ad8330bb2214a29a45aa7060a2e70bd038ae64
Harivansh <hello@harivan.sh>
2021-04-04 18:37:25 +0400

    major refactoring

--- main.py
+++ main.py
@@ -1,103 +1,17 @@
 #!/usr/bin/env python3
 
-from os import getuid, getgid, getcwd
-from subprocess import run
-from typing import Mapping, Union
-
-import modules.git as git
-from modules.extracts import generate_configuration_files, parser, preliminary_checks, scaffold_directory_structure, \
-    validate_script_arguments
-from modules.utilities import cd
+from modules.configuration import ConfigurationAccessorType
+from modules.extracts import configure, countdown, ignition, main_engine_start, preflight_checks, takeoff
 
 if __name__ == '__main__':
-    preliminary_checks(requirements={
-        'docker.version': '20.10.5',
-        'docker-compose.version': '1.28.0',
-        'openssl.version': '1.1.1',
-        'git.version': '2.31.0',
-    })
+    preflight_checks()
 
-    validate_script_arguments(arguments := parser().parse_args())
+    configuration: ConfigurationAccessorType = configure()
 
-    configuration: Mapping[str, Union[str, int]] = {
-        'project.name': arguments.project_name,
-        'project.domain': arguments.domain,
+    ignition(configuration)
 
-        'services.nginx.ssl.certificate.name': 'certificate.pem',
-        'services.nginx.ssl.key.name': 'key.pem',
+    main_engine_start(configuration)
 
-        'services.postgres.database': arguments.project_name.lower(),
-        'services.postgres.username': 'username',
-        'services.postgres.password': 'password',
+    countdown(configuration)
 
-        'services.adminer.port': 8080,
-        'services.mailhog.port': 8025,
-
-        'miscellaneous.node.image.tag': 'stretch',
-    }
-
-    scaffold_directory_structure({
-        configuration['project.name']: {
-            # docker-compose.yml
-            # .gitignore
-            # README.md
-
-            'configuration': {
-                'nginx': {
-                    'conf.d': {
-                        # default.conf
-                        # utils.conf
-                    },
-                    'ssl': {
-                        # [ssl key]
-                        # [ssl certificate]
-                    }
-                },
-                'supervisor': {
-                    'conf.d': {
-                        # supervisord.conf
-                    }
-                }
-            },
-            'docker-compose': {
-                'services': {
-                    'php': {
-                        # Dockerfile
-                    }
-                }
-            },
-            'application': {
-                'core': {
-                    # [laravel application]
-                }
-            }
-        }
-    })
-
-    with cd(configuration['project.name']):
-        generate_configuration_files(configuration)
-
-        with cd('application/core'):
-            run(
-                (
-                    'docker', 'run',
-                    '--rm',
-                    '--interactive',
-                    '--tty',
-                    '--user',
-                    f'{getuid()}:{getgid()}',
-                    '--mount', f'type=bind,source={getcwd()},target=/application',
-                    '--workdir', '/application',
-                    'composer', 'create-project',
-                    '--prefer-dist',
-                    '--ignore-platform-reqs',
-                    'laravel/laravel', configuration['project.name']
-                ),
-                check=True
-            )
-
-            with cd(configuration['project.name']):
-                git.init()
-                git.add(('.',))
-                git.commit('initial commit')
-                git.checkout_to_new_branch('development')
+    takeoff(configuration)
--- modules/extracts.py
+++ modules/extracts.py
@@ -2,30 +2,36 @@
 This module contains code extracted from other scripts (mostly main.py)
 """
 
-from argparse import ArgumentParser, Namespace
-from os import getuid, getgid
+from os import getuid, getgid, getcwd
 from pathlib import Path
 from shutil import copyfile
 from string import Template
-from typing import Any, Mapping, Union
+from subprocess import run
+from typing import Mapping
 
-from modules.scaffolding import directory_structure_is_valid, create_directory_structure, \
-    generate_self_signed_tls_certificate
+from modules.configuration import ConfigurationAccessorType
+from modules.configuration import create_argument_parser, validated_script_arguments, create_configuration_accessor
+from modules.generators import setup_directory_structure, generate_self_signed_tls_certificate
 from modules.utilities import cd, template_path
-from modules.verification import correct_version_is_installed, directory_exists, domain_is_valid, is_pascal_case
+from modules.verification import correct_version_is_installed
 
 
-def preliminary_checks(*, requirements: Mapping[str, str]) -> None:
+def preflight_checks() -> None:
     """
     Checks whether the correct version of the dependencies of the script are installed.
-
-    Args:
-        requirements: Versions of the dependencies
     """
+    requirements: Mapping[str, str] = {
+        'docker.version': '20.10.5',
+        'docker-compose.version': '1.28.0',
+        'openssl.version': '1.1.1',
+        'git.version': '2.31.0',
+    }
+
     if not correct_version_is_installed(
             ('docker', 'version', '--format', '{{.Server.Version}}'), requirements['docker.version']):
         raise RuntimeError(
-            f"The correct docker version is not installed. Docker >= v{requirements['docker.version']} is needed."
+            'The correct docker version is not installed. '
+            f"Docker >= v{requirements['docker.version']} is needed."
         )
 
     if not correct_version_is_installed(
@@ -38,7 +44,8 @@ def preliminary_checks(*, requirements: Mapping[str, str]) -> None:
     if not correct_version_is_installed(
             ('openssl', 'version'), requirements['openssl.version']):
         raise RuntimeError(
-            f"The correct openssl version is not installed. OpenSSL >= v{requirements['openssl.version']} is needed."
+            'The correct openssl version is not installed. '
+            f"OpenSSL >= v{requirements['openssl.version']} is needed."
         )
 
     if not correct_version_is_installed(
@@ -48,142 +55,124 @@ def preliminary_checks(*, requirements: Mapping[str, str]) -> None:
         )
 
 
-def parser() -> ArgumentParser:
+def configure() -> ConfigurationAccessorType:
     """
-    Create an argument parser for all the script's arguments.
-
-    Returns:
-        The customized ArgumentParser object.
+    Instantiates a configuration accessor for the project.
     """
-    main_parser: ArgumentParser = ArgumentParser(
-        prog='laravel',
-        description='A script to scaffold laravel projects.',
-        allow_abbrev=False
-    )
-
-    setup_subparser: ArgumentParser = main_parser.add_subparsers(
-        title='Action',
-        description='The action to take.',
-        dest='action',
-        required=True
-    ).add_parser(
-        'setup',
-        description='Setup a new laravel project.',
-        add_help=True,
-        allow_abbrev=False
-    )
-
-    setup_subparser.add_argument(
-        'project_name',
-        help='The name of the laravel project to scaffold.'
-    )
-    setup_subparser.add_argument(
-        '--domain',
-        default='application.local',
-        help='The domain name where the project will be hosted.'
-    )
-    setup_subparser.add_argument(
-        '--with',
-        nargs='*',
-        choices=('horizon', 'sanctum', 'scout', 'socialite', 'telescope'),
-        help='Additional packages to install.'
-    )
-
-    return main_parser
-
-
-def validate_script_arguments(arguments: Namespace) -> None:
-    if not is_pascal_case(arguments.project_name):
-        raise RuntimeError(f"The project name: '{arguments.project_name}' is not pascal-cased.")
-
-    if directory_exists(arguments.project_name):
-        raise RuntimeError(
-            f"The directory: '{arguments.project_name}' already exists in the current working directory.")
-
-    if not domain_is_valid(arguments.domain):
-        raise RuntimeError(f"The domain: '{arguments.domain}' is invalid.")
-
-
-def scaffold_directory_structure(directory_structure: Mapping[str, Any]) -> None:
-    if not directory_structure_is_valid(directory_structure):
-        raise RuntimeError("The project's provided directory structure is invalid.")
-
-    create_directory_structure(directory_structure)
-
-
-def generate_configuration_files(configuration: Mapping[str, Union[str, int]]) -> None:
-    with cd('configuration/nginx/ssl'):
-        generate_self_signed_tls_certificate(
-            certificate_name=configuration['services.nginx.ssl.certificate.name'],
-            key_name=configuration['services.nginx.ssl.key.name'],
-            domain=configuration['project.domain']
+    return create_configuration_accessor(
+        **validated_script_arguments(
+            create_argument_parser().parse_args()
         )
+    )
 
-    with open('docker-compose.yml', 'w') as file, open(f"{template_path('docker-compose.yml')}") as template:
-        file.write(
-            Template(template.read()).substitute({
-                'PROJECT_NAME': configuration['project.name'],
-                'USER_ID': getuid(),
-                'GROUP_ID': getgid(),
-                'POSTGRES_DB': configuration['services.postgres.database'],
-                'POSTGRES_USER': configuration['services.postgres.username'],
-                'POSTGRES_PASSWORD': configuration['services.postgres.password'],
-                'ADMINER_PORT': configuration['services.adminer.port'],
-                'MAILHOG_PORT': configuration['services.mailhog.port'],
-            })
-        )
 
-    with open('run', 'w') as file, open(f"{template_path('run')}") as template:
-        file.write(
-            Template(template.read()).substitute({
-                'PROJECT_NAME': configuration['project.name'],
-                'NODE_IMAGE_TAG': configuration['miscellaneous.node.image.tag'],
-            })
-        )
+def ignition(configuration: ConfigurationAccessorType) -> None:
+    setup_directory_structure(configuration('project.name'))
 
-    Path('run').chmod(0o755)
 
-    copyfile(template_path('.gitignore'), f'{Path.cwd()}/.gitignore')
-
-    with open('README.md', 'w') as file, open(f"{template_path('README.md')}") as template:
-        file.write(
-            Template(template.read()).substitute({
-                'PROJECT_NAME': configuration['project.name'],
-                'PROJECT_DOMAIN': configuration['project.domain'],
-                'SSL_KEY_NAME': configuration['services.nginx.ssl.key.name'],
-                'SSL_CERTIFICATE_NAME': configuration['services.nginx.ssl.certificate.name'],
-                'ADMINER_PORT': configuration['services.adminer.port'],
-                'MAILHOG_PORT': configuration['services.mailhog.port'],
-            })
-        )
-
-    with cd('configuration'):
-        with cd('nginx/conf.d'):
-            with open('default.conf', 'w') as file, \
-                    open(f"{template_path('configuration/nginx/conf.d/default.conf')}") as template:
-                file.write(
-                    Template(template.read()).substitute({
-                        'PROJECT_DOMAIN': configuration['project.domain'],
-                        'SSL_KEY_NAME': configuration['services.nginx.ssl.key.name'],
-                        'SSL_CERTIFICATE_NAME': configuration['services.nginx.ssl.certificate.name'],
-                    })
-                )
-
-            with open('utils.conf', 'w') as file, \
-                    open(f"{template_path('configuration/nginx/conf.d/utils.conf')}") as template:
-                file.write(
-                    Template(template.read()).substitute({
-                        'PROJECT_DOMAIN': configuration['project.domain'],
-                        'ADMINER_PORT': configuration['services.adminer.port'],
-                        'MAILHOG_PORT': configuration['services.mailhog.port'],
-                    })
-                )
-
-        with cd('supervisor/conf.d'):
-            copyfile(
-                template_path('configuration/supervisor/conf.d/supervisord.conf'),
-                f'{Path.cwd()}/supervisord.conf'
+def main_engine_start(configuration: ConfigurationAccessorType) -> None:
+    with cd(configuration('project.name')):
+        with cd('configuration/nginx/ssl'):
+            generate_self_signed_tls_certificate(
+                domain=configuration('project.domain'),
+                certificate_name=configuration('services.nginx.ssl.certificate.name'),
+                key_name=configuration('services.nginx.ssl.key.name')
             )
 
-    with cd('docker-compose/services/php'):
-        copyfile(template_path('docker-compose/services/php/Dockerfile'), f'{Path.cwd()}/Dockerfile')
+        with open('docker-compose.yml', 'w') as file, open(f"{template_path('docker-compose.yml')}") as template:
+            file.write(
+                Template(template.read()).substitute({
+                    'PROJECT_NAME': configuration('project.name'),
+                    'USER_ID': getuid(),
+                    'GROUP_ID': getgid(),
+                    'POSTGRES_DB': configuration('services.postgres.database'),
+                    'POSTGRES_USER': configuration('services.postgres.username'),
+                    'POSTGRES_PASSWORD': configuration('services.postgres.password'),
+                    'ADMINER_PORT': configuration('services.adminer.port'),
+                    'MAILHOG_PORT': configuration('services.mailhog.port'),
+                })
+            )
+
+        with open('run', 'w') as file, open(f"{template_path('run')}") as template:
+            file.write(
+                Template(template.read()).substitute({
+                    'PROJECT_NAME': configuration('project.name'),
+                    'NODE_IMAGE_TAG': configuration('miscellaneous.node.image.tag'),
+                })
+            )
+
+        Path('run').chmod(0o755)
+
+        copyfile(template_path('.gitignore'), f'{Path.cwd()}/.gitignore')
+
+        with open('README.md', 'w') as file, open(f"{template_path('README.md')}") as template:
+            file.write(
+                Template(template.read()).substitute({
+                    'PROJECT_NAME': configuration('project.name'),
+                    'PROJECT_DOMAIN': configuration('project.domain'),
+                    'SSL_KEY_NAME': configuration('services.nginx.ssl.key.name'),
+                    'SSL_CERTIFICATE_NAME': configuration('services.nginx.ssl.certificate.name'),
+                    'ADMINER_PORT': configuration('services.adminer.port'),
+                    'MAILHOG_PORT': configuration('services.mailhog.port'),
+                })
+            )
+
+        with cd('configuration'):
+            with cd('nginx/conf.d'):
+                with open('default.conf', 'w') as file, \
+                        open(f"{template_path('configuration/nginx/conf.d/default.conf')}") as template:
+                    file.write(
+                        Template(template.read()).substitute({
+                            'PROJECT_DOMAIN': configuration('project.domain'),
+                            'SSL_KEY_NAME': configuration('services.nginx.ssl.key.name'),
+                            'SSL_CERTIFICATE_NAME': configuration('services.nginx.ssl.certificate.name'),
+                        })
+                    )
+
+                with open('utils.conf', 'w') as file, \
+                        open(f"{template_path('configuration/nginx/conf.d/utils.conf')}") as template:
+                    file.write(
+                        Template(template.read()).substitute({
+                            'PROJECT_DOMAIN': configuration('project.domain'),
+                            'ADMINER_PORT': configuration('services.adminer.port'),
+                            'MAILHOG_PORT': configuration('services.mailhog.port'),
+                        })
+                    )
+
+            with cd('supervisor/conf.d'):
+                copyfile(
+                    template_path('configuration/supervisor/conf.d/supervisord.conf'),
+                    f'{Path.cwd()}/supervisord.conf'
+                )
+
+        with cd('docker-compose/services/php'):
+            copyfile(template_path('docker-compose/services/php/Dockerfile'), f'{Path.cwd()}/Dockerfile')
+
+
+def countdown(configuration: ConfigurationAccessorType) -> None:
+    with cd(f"{configuration('project.name')}/application/core"):
+        run(
+            (
+                'docker', 'run',
+                '--rm',
+                '--interactive',
+                '--tty',
+                '--user',
+                f'{getuid()}:{getgid()}',
+                '--mount', f'type=bind,source={getcwd()},target=/application',
+                '--workdir', '/application',
+                'composer', 'create-project',
+                '--prefer-dist',
+                '--ignore-platform-reqs',
+                'laravel/laravel', configuration('project.name'),
+            ),
+            check=True
+        )
+
+
+def takeoff(configuration: ConfigurationAccessorType) -> None:
+    with cd(f"{configuration('project.name')}/application/core/{configuration('project.name')}"):
+        run(('git', 'init'), check=True)
+        run(('git', 'add', '*'), check=True)
+        run(('git', 'commit', '--message', 'initial commit'), check=True)
+        run(('git', 'checkout', '-b', 'development'), check=True)
